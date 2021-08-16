6bfe4c22d648d7443836dd119562d300711c86af
Andreas Ink <andreasink17@gmail.com>
2021-08-16 10:27:30 -0400

    Commented on the intro code

--- hello.c
+++ hello.c
@@ -1,10 +1,17 @@
+/* includes a library to import basic functions like printf
+std stands for standard */
 #include <stdio.h>
 
+/* int is a data type that needs to be returned, main is an indentifier, the whole thing is a function */ 
 int main()
 {
+/* char stands for charcter, add [] after name of var to include multiple charcters
+all statments have colons */ 
 char name[] = "John";
 int age = 34;
 
+/* printf is a function imported by the standard library, %s inserts a string at that is declared at the end of the print func, %i does the same but its an integer */ 
 printf("%s is %i", name, age);
+/* functions need to return something to show it worked */ 
 return 0;
 }
